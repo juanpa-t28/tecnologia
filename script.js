8cf42b6c4f093af032534c7ec9c08fc4d8f6062d
juanpa-t28 <77943538+juanpa-t28@users.noreply.github.com>
2021-03-05 14:42:14 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var array=[];
+var i=0;
+var	mayor;
+var a=document.getElementById('entrada');
+function fingresa(){
+	if(i<5)
+	{
+		if (i==0){
+			mayor=parseInt(a.value);
+		}
+		else if(a.value>mayor)
+		{
+			mayor=parseInt(a.value);
+		}
+		console.log(mayor);
+		i+=1;
+		array[i]=a.value;
+	}
+
+	console.log(array);
+		
+}
+
+function fmostrar()
+{
+	ds=document.getElementById('resultado');
+	ds.value=mayor;
+}
